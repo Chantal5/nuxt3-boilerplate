42509e6950df954c31b497d28d910a140b100799
Chantal Sloep <chantalsloep@outlook.com>
2024-09-10 20:49:16 +0200

    Added boilerplate sitemap.xml script

---
+++ server/routes/sitemap.xml.ts
@@ -0,0 +1,23 @@
+import { SitemapStream, streamToPromise } from 'sitemap'
+
+export default defineEventHandler(async (event) => {
+  const sitemap = new SitemapStream({
+    hostname: 'https://www.putyourownwebsitehere.com'
+  })
+
+  //Set urls variable and add homepage
+  const urls = [
+    '/',
+  ]
+
+  for (const doc of urls) {
+    sitemap.write({
+      url: doc,
+      changefreq: 'monthly'
+    })
+  }
+
+  sitemap.end()
+
+  return streamToPromise(sitemap)
+})
