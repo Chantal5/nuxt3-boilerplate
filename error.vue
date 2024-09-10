0399f706bbe3168f9982eb2fd9cc91349a73664b
Chantal Sloep <chantalsloep@outlook.com>
2024-09-10 20:51:54 +0200

    Added error page

---
+++ error.vue
@@ -0,0 +1,12 @@
+<template>
+  <NuxtLayout>
+    <h1>{{ error.statusCode }}</h1>
+    <p>Something went wrong...</p>
+  </NuxtLayout>
+</template> 
+
+<script setup>
+  import '@/assets/css/style.scss'
+  
+  const error = useError();
+</script>
