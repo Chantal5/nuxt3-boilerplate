9c249f9971f6eb3abd2c147a255b47cc9f3b5765
Chantal Sloep <chantalsloep@outlook.com>
2024-09-10 20:52:18 +0200

    Added pages

---
+++ pages/about.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="box">
+    <h1>Nuxt 3 boilerplate - About page</h1>
+    <p>This is a second page. Use it, or remove it and make your own.</p>
+  </div>
+</template>
+
+<script setup>
+  //Apply css classes to the body
+  useHead({
+    bodyAttrs: {
+      class: 'theme--pink'
+    },
+  })
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="box">
+    <h1>Nuxt 3 boilerplate</h1>
+    <p>This Nuxt 3 application is stille empty. You can start here to build your own pages and components.</p>
+
+    <h2 v-if="viewport.isLessThan('tablet')">You can only see me on your mobile device!</h2>
+  </div>
+</template>
+
+<script setup>
+  const viewport = useViewport()
+</script>
+
+<style lang="scss" scoped>
+  //Use inline styling as little as possible, every vue file with a styling block in it, creates a render blocking css file.
+  //For best performance, use only one stylesheet. An example is located in /assets/css
+  //Scoped styling example. Only content of this file is targeted. You can see the h1 on the about page is still default black.
+  h1 {
+    color: red
+  }
+</style>
